6f35f95bbf42b2354b07c394af26091838332f04
Oscar Bautista <oscar.adba@gmail.com>
2021-10-23 17:15:24 -0700

    Add styles to props

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -6,7 +6,15 @@ const Cards = () => {
   return (
     <>
       {cards.map(({ lang, url, fcolor, scolor }) => {
-        return <Card key={lang} lang={lang} img={url} />;
+        return (
+          <Card
+            key={lang}
+            lang={lang}
+            img={url}
+            fcolor={fcolor}
+            scolor={scolor}
+          />
+        );
       })}
     </>
   );
