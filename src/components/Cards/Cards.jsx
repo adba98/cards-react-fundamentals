b805bea5cee1de90035f2171b079a2aad619bdd8
Oscar Bautista <oscar.adba@gmail.com>
2021-10-23 17:00:32 -0700

    Add card component from cards

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "../Card/Card";
+import cards from "../../models/cards";
+
+const Cards = () => {
+  return (
+    <>
+      {cards.map(({ lang, url, fcolor, scolor }) => {
+        return <Card key={lang} lang={lang} img={url} />;
+      })}
+    </>
+  );
+};
+
+export default Cards;
